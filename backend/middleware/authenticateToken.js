f78bcddcd13e459c441d0e347554e17e68137ed3
Edward2001-create <leecrafton@outlook.com>
2026-07-29 21:06:03 -0500

    produced secured GET and POST routes

---
+++ backend/middleware/authenticateToken.js
@@ -0,0 +1,18 @@
+const jwt = require("jsonwebtoken");
+
+function authenticateToken(req, res, next) {
+    const authHeader = req.headers["authorization"];
+    const token = authHeader && authHeader.split(" ")[1];
+    if (!token) {
+        return res.status(401).json({message: "access denied. No token provided."});
+    }
+    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(403).json({message: "invalid or expired token."});
+        }
+        req.user = decoded;
+        next();
+    });
+}
+
+module.exports = authenticateToken;
